ebc1a1997ff9dde824bbca94b4863814b20e51d1
purblue <purblue@umich.edu>
2015-04-20 04:47:52 -0400

    ensemble

--- ensemble.py
+++ ensemble.py
@@ -1,3 +1,4 @@
+from scipy import sparse
 import numpy as np
 import csv
 import re 
@@ -25,10 +26,10 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LogisticRegressionCV
 
-
-train_path = "./data/train.json"
-test_path = "./data/test.json"
+train_path = "./data/data2/train.json"
+test_path = "./data/data2/test.json"
 
 
 train1, train2, train3 = dp.form_matrix(train_path, type=0)
@@ -42,7 +43,7 @@ test_X1 = [ row[1][0:1]+row[1][2:]  for row in test1]
 test_y = [ row[0]  for row in test1]
 
 train_X1 = np.array(train_X1)
-test_X1 = test_X1.toarray()
+test_X1 = np.array(test_X1)
 train_y = np.array(train_y)
 test_y = np.array(test_y)
 
@@ -80,26 +81,85 @@ test_X3 = vectorizer.transform(test_text)
 ch2, train_X3_ch2, test_X3_ch2 = fs.chisq(train_X3, train_y3, test_X3, 20000)
 
 
-#2. Classifier
-logit1 = LogisticRegression(penalty="l2", dual=True, C=10)
-logit1.fit(train_X1, train_y)
-#score_logit = misc.testCV(logit1, train_X1, train_y, 5)
+train_X1 = sparse.csr_matrix(train_X1)
+train_X1 = train_X1.toarray()
 
-logit2 = LogisticRegression(penalty="l2", dual=True, C=500)
+train_X_tot = sparse.hstack((sparse.csr_matrix(train_X1), sparse.csr_matrix(train_X2), train_X3_ch2))
+test_X_tot = sparse.hstack((sparse.csr_matrix(test_X1), sparse.csr_matrix(test_X2), test_X3_ch2))
+
+#2. Classifier
+logit1 = LogisticRegression(penalty="l2", dual=False, C=1)
+logit1.fit(train_X1, train_y)
+
+logit2 = LogisticRegression(penalty="l2", dual=True, C=1)
 logit2.fit(train_X2, train_y)
 
-logit3 = LogisticRegression(penalty="l2", dual=True, C=500)
+logit3 = LogisticRegression(penalty="l2", dual=True, C=5)
 logit3.fit(train_X3_ch2, train_y)
 
 #3. prediction
 predict1 = logit1.predict(test_X1)
-result1 = misc.evaluation(predict1.tolist(), test_y)
-
 predict2 = logit2.predict(test_X2)
-result2 = misc.evaluation(predict2.tolist(), test_y)
-
 predict3 = logit3.predict(test_X3_ch2)
+
+result1 = misc.evaluation(predict1.tolist(), test_y)
+result2 = misc.evaluation(predict2.tolist(), test_y)
 result3 = misc.evaluation(predict3.tolist(), test_y)
+
+# Total Accuracy: 0.76086 
+# 0-label Accuracy: 0.77592 (4536 / 5846)
+# 1-label Accuracy: 0.68458 (790 / 1154)
+
+predict1_prob = logit1.predict_proba(test_X1)
+predict2_prob = logit2.predict_proba(test_X2)
+predict3_prob = logit3.predict_proba(test_X3_ch2)
+r = majority_prob(predict1_prob, predict2_prob, predict3_prob, [0.3, 0.05, 0.65])
+result4 = misc.evaluation(r, test_y)
+
+r = majority(result1, result2, result3)
+result4 = misc.evaluation(r, test_y)
+def majority(result1, result2, result3):
+	major = []
+	for i in range(len(result1)):
+		tp = (result1[i][1], result2[i][1], result3[i][1])
+		nTrue = len([ k for k in tp if k==1])
+		nFalse = len([ k for k in tp if k==0])
+		m = 1 if nTrue>nFalse else 0
+		major.append(m)
+	return major
+
+def majority_prob(predict1_prob, predict2_prob, predict3_prob, w):
+	prob1 = [ row[1] for row in predict1_prob]
+	prob2 = [ row[1] for row in predict2_prob]
+	prob3 = [ row[1] for row in predict3_prob]
+	result = []
+	for i in range(len(prob1)):
+		p = w[0]*prob1[i] + w[1]*prob2[i] + w[2]*prob3[i]
+		label = 1 if p > 0.5 else 0
+		result.append(label)
+	return result
+
+
+train_X_tot
+
+score_logit = misc.testCV(logit5, train_X_tot, train_y, 5)
+
+logit5 = LogisticRegression(penalty="l2", dual=False, C=10)
+logit5.fit(train_X_tot, train_y)
+predict5 = logit5.predict(test_X_tot)
+result5 = misc.evaluation(predict5.tolist(), test_y)
+# logit_grid = LogisticRegressionCV(Cs=[2000, 2500, 3000, 3500, 4000], cv=5, dual=False, penalty="l2")
+# logit_grid.fit(train_X3_ch2, train_y)
+
+# ev = []
+# for i in range(len(logit_grid.scores_[1])):
+# 	ev.append(np.array([row[i] for row in logit_grid.scores_[1]]).mean())
+
+# logit_grid.C_
+# logit_grid.Cs_
+# ev
+
+
 
 
 ch2, train_X_ch2, test_X_ch2 = fs.chisq(train_X, train_y, test_X, 5000)
